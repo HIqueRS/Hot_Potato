8a747a1754b842c06dc3e01f717c2625ace35859
Henrique Souza de Lima <riquesouzalima11@gmail.com>
2020-04-18 12:05:54 -0300

    Two new assets

---
+++ Hot_Potato/Assets/Scripts/LightManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomEditor(typeof(LightManager))]
+public class LightManagerEditor : Editor
+{    
+    Light[] lights;
+    float intensity;
+
+    private void OnEnable()
+    {
+        lights = GameObject.FindObjectsOfType<Light>();
+      
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+
+        EditorGUILayout.Slider(0f, 0f, 1f);
+
+
+
+
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+#endif
+public class LightManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
