e7af2481a00b6e2bf13c735bf7e6854e0fa386da
Henrique Souza de Lima <riquesouzalima11@gmail.com>
2020-04-19 09:22:16 -0300

    a batata tá andando

---
+++ Hot_Potato/Assets/Scripts/MovePotato.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovePotato : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            Debug.Log("fiu");
+
+            if(Physics.Raycast(ray,out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
