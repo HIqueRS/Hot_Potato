f0d394dac500a35ea7a27ef246ef0ea28bfd5bf2
Aline Pessoa da Silva Kaiper <lilica.kaiper@gmail.com>
2020-04-19 16:26:49 -0300

    Agora Bota

--- Hot_Potato/Assets/Scripts/ChangeBar.cs
+++ Hot_Potato/Assets/Scripts/ChangeBar.cs
@@ -9,7 +9,7 @@ public class ChangeBar : MonoBehaviour
 
     public Image[] imageLuz;
     public Image[] imageFogo;
-    public GameManager game;
+    private GameManager game;
     public Light[] luz;
 	public Light[] fogo;
     private float[] maxIntensityLuz;
@@ -25,25 +25,37 @@ public class ChangeBar : MonoBehaviour
 		taLigadaLuz = new bool[2];
 		taLigadoFogo = new bool[2];
 
+		maxIntensityFogo = new float[2];
+		maxIntensityLuz = new float[2];
+
         maxIntensityLuz[0] = luz[0].intensity;
 		maxIntensityLuz[1] = luz[1].intensity;
 		maxIntensityFogo[0] = fogo[0].intensity;
 		maxIntensityFogo[1] = fogo[1].intensity;
 
 		manager = Resources.Load<GameObject>("GameManager");
+		game = Resources.Load<GameManager>("GameManager");
     }
 
     // Update is called once per frame
     void Update()
     {
 
-
+		MudaLuzEBarra(0);
 		MudaLuzEBarra(1);
+		MudaFogoEBarra(0);
+		MudaFogoEBarra(1);
+		
     }
 
 	void MudaLuzEBarra(int num)
 	{
         luz[num].intensity -= decreaseTime * Time.deltaTime;
         game.intensityLuz[num] = imageLuz[num].fillAmount = luz[num].intensity/ maxIntensityLuz[num];
+	}
+	void MudaFogoEBarra(int num)
+	{
+        fogo[num].intensity -= decreaseTime * Time.deltaTime;
+        game.intensityFogo[num] = imageFogo[num].fillAmount = fogo[num].intensity/ maxIntensityFogo[num];
 	}
 }
