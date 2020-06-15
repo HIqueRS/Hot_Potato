c9f82edb190c27941b89fe5e66c708662a521411
Henrique Souza de Lima <riquesouzalima11@gmail.com>
2020-06-15 19:15:42 -0300

    passo 1 shader

--- Hot_Potato/Assets/Scripts/OrthoCam.cs
+++ Hot_Potato/Assets/Scripts/OrthoCam.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class OrthoCam : MonoBehaviour
 {
+    public Camera cam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void Test()
+    {
+        // cam.orthographic = true;
         
     }
 }
